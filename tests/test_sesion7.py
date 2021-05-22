da7a961ba5f2906849eb922b8576a1e73d8c0189
Anabel Palasí <anapalasi@ieshonorigarcia.net>
2021-05-22 16:55:23 +0200

    tests

---
+++ tests/test_sesion7.py
@@ -0,0 +1,10 @@
+import sesion7
+
+
+def test_says_world():
+    assert sesion7.suma(1,2) == 3
+    assert sesion7.resta(3,2) == 1
+    assert multiplicacion(3,4) == 12
+    assert raizcuadrada(16) == 4
+    assert modulo(5,2) == 1
+    assert elevado(4,2) == 8
