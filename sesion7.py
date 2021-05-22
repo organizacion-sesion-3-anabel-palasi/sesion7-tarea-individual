174c447dc0b87067e3dbcfafedc68455e8c3b052
Anabel <anapalasi@gmail.com>
2021-05-22 16:50:00 +0200

    Create sesion7.py

---
+++ sesion7.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import sys
+
+# Calcula la suma
+def suma(num1, num2):
+  pass
+# Calcula la resta
+def resta(num1, num2):
+  pass
+
+# Calcula la multiplicacion
+def multiplicacion(num1, num2):
+  pass
+
+# Calcula la raiz cuadrada
+def raizcuadrada(numero):
+  pass
+
+# Calcula el módulo
+def modulo(num1,num2):
+  pass
+
+# Calcula x elevado a y
+def elevado(num1, num2):
+  pass
+  
